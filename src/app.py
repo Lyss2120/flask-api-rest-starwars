3cac915441f0673202b6321bbbfe6684bcf97995
Lyss2120 <Lys.ivonne@gmail.com>
2022-12-23 06:13:34 +0000

    endpoints people listos

--- src/app.py
+++ src/app.py
@@ -96,52 +96,53 @@ def get_one_planet(planet_id):
 
 @app.route('/users/favorites', methods=['GET'])
 def get_favorites():
-    body = request.get_json()  # se envia el id del usuario en el body al hacer el request {"id":1}
-    user_id=body['id']#[]para acceder las propiedades del dictionary
-    current_user= User.query.get(user_id)
-    current_user= current_user.__repr__()#muestra el nombre del current_user ej: <user : 'admin'>
+    # enviar el id del usuario en el body al hacer el request {"id":1}
+    user_id = request.get_json()['id']#[]para acceder las propiedades del body
+    current_user = User.query.get(user_id).__repr__()
+    # favorite_people = Fav_people.query.all()#trayendo todos los favoritos de todos los usuarios
+    # favorite_people_ser = list(map(lambda x: x.serialize(), favorite_people))#para convertirlos en diccionarios y mostrar sus valores #print("id en la posicion 1:",favorite_people_ser[1]['user'])#accediendo al id_usuario dentro del primer favorito registrado 
+    # all_user_fav_people = list(map(lambda x: x['user'], favorite_people_ser))# muestra todos los id_usuario
+    fav_people_by_userid = Fav_people.query.filter_by(user=user_id)#filtra solo los favoritos que contengan el user id del current user
+    user_fav_people = list(map(lambda x: x.serialize(), fav_people_by_userid))
+    # peoplefav=user_fav_people[2]['people']#accede al id del people agregado como favorito ej:4
+    people_id = list(map(lambda x: x['people'] , user_fav_people))#accede  a los ids de los people, tengo que elegir de tabla People esos ids y acceder al name de cada id, con un map?
+    people_name = list(map(lambda x: People.query.get(x) , people_id))# acceder al name de cada id, [People 'Luke Skywalker', People 'crpo', People 'r2d2']
+    #people_name = People.query.filter_by(id=peoplen) aún no funciona no lo toma 
+    print(people_name)
+    fav_planets_by_userid = Fav_planets.query.filter_by(user=user_id)
+    user_fav_planets = list(map(lambda x: x.serialize(), fav_planets_by_userid))
 
-    favorite_people = Fav_people.query.all()#trayendo todos los favoritos de todos los usuarios
-    favorite_people_ser = list(map(lambda x: x.serialize(), favorite_people))#para convertirlos en diccionarios y mostrar sus valores
-    # if favorite_people_ser
-    #print("id en la posicion 1:",favorite_people_ser[1]['user'])#accediendo al id_usuario dentro del primer favorito registrado 
-    findingwaldo = list(map(lambda x: x['user'], favorite_people_ser))# muestra todos los id_usuario
-    print(findingwaldo)
+    return jsonify({"current user": current_user,
+                    "favorite people": user_fav_people,
+                    "favorite planets": user_fav_planets,
+                    # "exp":people_name sale TypeError: Object of type People is not JSON serializable
+                    })
 
-#favorite_people_ser es una lista de dictionarys el map accede a la lista y el [] accede al key value del dictionary
-    # favorite_planets = Fav_planets.query.all()
-    # favorite_planets_ser = list(map(lambda x: x.serialize(), favorite_planets))
-
-    # return jsonify({
-    #     "favorite people": favorite_people_ser, 
-    #     "favorite planets": favorite_planets_ser
-    #     })
-    return jsonify({"current user":current_user})
 
 @app.route('/favorite/people/<int:people_id>', methods=['POST'])
 def modify_user_favorite(people_id):
-    if body is None:
-            return "The request body is null", 400
-    if 'id' not in body:
-            return 'You need to specify the id',400
-    else:
-        body = request.get_json()  # conseguir los datos del usuario
+        user = request.get_json()['id']  # conseguir el user _id que se envió en el body
+    # if user is None:
+    #         return "The request user is null", 400
+    # if 'id' not in user:
+    #         return 'You need to specify the id',400
+    # else:
         character = People.query.get(people_id)  # personaje asociado al people_id
-        new_favorite = Fav_people(user=body['id'], people=people_id)
+        new_favorite = Fav_people(user=user, people=people_id)
         db.session.add(new_favorite)
         db.session.commit()
         return jsonify({
             "added to favorites": character.__repr__(),
             "id": people_id
         })
-
+        
    
 @app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
 def delete_favorite(people_id):
-    delete_fav = Fav_people.query.get(people_id)
-    db.session.delete(delete_fav)
+    deleted = Fav_people.query.filter_by(people=people_id).first()#filtra solo los people_fav que contengan el people_id
+    db.session.delete(deleted)
     db.session.commit()
-    return jsonify({"deleted from favorites" })
+    return jsonify({"deleted from favorites": deleted.serialize()})
 
 
 # this only runs if `$ python src/app.py` is executed
